f80560a4b0831abdf71fb38b9cd2bfd207d50e51
fyztk <35735@qq.com>
2016-06-08 19:59:17 +0800

    11

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,17 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-
+$api = app('Dingo\Api\Routing\Router');
 $app->get('/', function () use ($app) {
     return $app->version();
+});
+$api->version('v1', ['namespace' => 'App\Http\Controllers\Api\V1'], function ($api) {
+    // 需要jwt验证后才能使用的API 也就是登陆之后,才能访问的路由,比如用户详细
+    $api->group(['middleware' => 'jwt.auth'], function ($api) {
+        #Auth
+        $api->get('auth/show', [
+            'as'   => 'auth.show',
+            'uses' => 'AuthController@show'
+        ]);
+    });
 });
