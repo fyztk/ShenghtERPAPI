8d0c9a25302610593a3bd315031dcff7b3b9d3bd
fyztk <35735@qq.com>
2016-06-11 21:01:49 +0800

    ztk

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Dingo\Api\Routing\Helpers;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    // 接口帮助调用
+    use Helpers;
+
+    // 请求
+    protected $request;
+
+    // 分页数
+    protected $perPage;
+
+    // 返回错误的请求
+    protected function errorBadRequest($message = '')
+    {
+        return $this->response->array($message)->setStatusCode(400);
+    }
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+        $this->perPage = $request->get('per_page');
+    }
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+    return $app->version();
+});
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1',function ($api){
+    $api->group(['namespace'=>'App\Api\V1\Controllers'],function ($api){
+        $api->post('user/login','AuthController@userLogin');
+        $api->post('user/register','AuthController@register');
+        $api->group(['middleware'=>'jwt.auth'],function($api){
+//            $api->get('user/me','AuthController@getAuthenticatedUser');
+//            $api->get('lessons','LessonsController@index');
+//            $api->get('lessons/{id}','LessonsController@show');
+        });
+
+    });
+});
